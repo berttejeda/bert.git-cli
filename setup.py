4b5e2cb552db0a49fe67c5729a0381f52aaee3b0
Bert Tejeda <berttejeda@gmail.com>
2025-11-24 14:20:26 -0500

    Added minimal setup script for uploading to pypi

---
+++ setup.py
@@ -0,0 +1,10 @@
+"""Setup script for ghsearch package.
+
+This is a minimal setup.py that allows using traditional build commands.
+The actual package configuration is in pyproject.toml.
+"""
+from setuptools import setup
+
+# setuptools will automatically read from pyproject.toml
+setup()
+
